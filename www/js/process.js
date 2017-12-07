11858d57274eef57cbd1da69b22ceb655bd39280
KieranOcal <kieran.ocal@yahoo.com>
2017-12-06 21:12:18 -0800

    finalapp

---
+++ www/js/process.js
@@ -0,0 +1,270 @@
+let process = {
+  menu:function(){
+    let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">9 Superheroes With Invincible Leadership Styles</div>
+              </div>
+            </div>
+
+    <div className="list-group">
+      <ul>
+      <li className="list-group-title"></li>
+        <li><a href='#' onClick={process.Superman}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Superman</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+      <li className="list-group-title"></li>
+        <li><a href='#' onClick={process.SpiderMan}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">SpiderMan</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+      <li className="list-group-title"></li>
+        <li><a href='#' onClick={process.Batman}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Batman</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+        <li className="list-group-title"></li>
+        <li><a href='#' onClick={process.IronMan}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">IronMan</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+        <li><a href='#' onClick={process.BlackWidow}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">BlackWidow</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+        <li><a href='#' onClick={process.CaptainAmerica}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">CaptainAmerica</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+        <li><a href='#' onClick={process.WonderWoman}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">WonderWoman</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+      </ul>
+    </div>
+
+</div>
+</div>
+</div>
+</div>
+</div>;
+    ReactDOM.render(content,document.getElementById('root'));
+  },
+  Alicorn:function(){
+    let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">World of Myth</div>
+              </div>
+            </div>
+            <center><img src={"img/imp.jpg"}/>
+            <p>asdajslfkjsldkjadlk jalkjlksjdlkaj lkj lakjskldj </p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+            ReactDOM.render(content,document.getElementById('root'));
+  },
+  Superman:function(){
+         let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Superman</div>
+              </div>
+            </div>
+            <center><img src={"img/superman.jpg"} width={"100%"}/>
+            <p>It’s a bird! It’s a plane! It’s a pretty awesome leader, if you ask me! One of the many reasons Superman is a powerful leader is his loyalty. He truly believes in the good of humanity and makes it his duty to defend it. If you can prove your loyalty to your team, they will respect you and follow you. Be a loyal and dependable leader.
+ </p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+         ReactDOM.render(content,document.getElementById('root'));
+  },
+  SpiderMan:function(){
+         let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">SpiderMan</div>
+              </div>
+            </div>
+            <center><img src={"img/spiderman.jpg"} width={"100%"}/>
+            <p>One of the most iconic superhero quotes comes from Spider-Man’s uncle Ben: “With great power comes great responsibility.” Spidey takes this message to heart, and it’s an important note for leaders to realize, as well. When you are in a leadership role, all your actions have consequences and you need to take responsibility for them. Like Spider-Man, you need to accept the responsibility that comes with power.
+ </p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+          ReactDOM.render(content,document.getElementById('root'));
+  },
+  Batman:function(){
+         let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Batman</div>
+              </div>
+            </div>
+            <center><img src={"img/batman.jpg"} width={"100%"}/>
+            <p>What makes Batman a fantastic leader is idea that Batman should be viewed as a symbol. If people put all of their faith in the idea of Batman, it doesn’t matter who is behind the mask. The fact that they believe in the idea of Batman is enough. When you lead a team, encourage them to believe in a unified goal. If you are successful, they won’t have to think much about who is leading them.
+ </p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+  IronMan:function(){
+         let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">IronMan</div>
+              </div>
+            </div>
+            <center><img src={"img/ironman.jpg"} width={"100%"}/>
+            <p> I’ve already written an entire article about Iron Man’s great leadership skills, so he clearly belongs on this list of heroes. One of the unique reasons Iron Man is a great leader is his transparency. Iron Man does not have a secret identity. Everyone knows he is Tony Stark. In fact, he announces it in a press conference. His honesty allows the people to trust him. If you want to lead like Iron Man, be transparent.
+ </p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+  BlackWidow:function(){
+         let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">BlackWidow</div>
+              </div>
+            </div>
+            <center><img src={"img/black.jpg"} width={"100%"}/>
+            <p>As a person without superpowers and the only female member of the Avengers, Black Widow has to work hard to step up as a leader. She might stick to herself, but her team values her opinion. She means business and she will get the job done. Even if you aren’t the most outspoken or impressive person in the room, you can still be a leader. Take a page out of Black Widow’s book. Hard work is one of the most essential leadership skills.
+ </p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+  CaptainAmerica:function(){
+  let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">CaptainAmerica</div>
+              </div>
+            </div>
+            <center><img src={"img/cap.jpg"} width={"100%"}/>
+            <p>Before he is Captain America, Steve Rogers is a scrawny kid who is often picked on by bullies. When he gains the super strength that makes him Captain America, he channels his experience into a mantra: stand up to bullies. His devotion to standing up to bullies and treating everyone with respect is what drives him as a superhero. Having strong beliefs like these can make you a great leader.</p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+    WonderWoman:function(){
+     let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">WonderWoman</div>
+              </div>
+            </div>
+            <center><img src={"img/wonder.jpg"} width={"100%"}/>
+            <p>As someone who values honesty, the Lasso of Truth is one of Wonder Woman’s most valuable assets. Like the name suggests, it forces people to tell the absolute truth. An effective team is completely honest with each other, so this is a key factor in Wonder Woman’s ability to lead. A leader who values honesty can accomplish great things.</p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+  loading:function(){
+    let content = <div>
+          Loading...
+        </div>;
+    ReactDOM.render(content,document.getElementById('root'));
+  }
+}
+
+process.loading();
+setTimeout(function(){
+  process.menu();
+},3000);
